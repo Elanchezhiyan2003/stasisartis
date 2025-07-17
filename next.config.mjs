bd462817178da8d190813ab60154c3ed8c8f9669
Elanchezhiyan M <chezhiyanmahi2020@gmail.com>
2025-07-17 10:36:35 +0530

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true
-  }
-}
+  // output: "export", // Make sure this is removed or commented out
 
-export default nextConfig
+  experimental: {
+    appDir: true,
+  },
+};
+
+export default nextConfig;
